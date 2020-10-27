3d7b60f54e43104317dd4c9023d37717d402d568
Rodrigo Acosta <jracosta1991@gmail.com>
2020-10-27 19:13:08 -0300

    agregando dashboard estadisticas

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
                 <a class="nav-link" href="/TPFinal/abmUsuario.php">Permisos <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Estadisticas</a>
+                <a class="nav-link" href="/TPFinal/estadistica.php">Estadisticas</a>
             </li>
             <!--
             <li class="nav-item">
