ed5303cd4f74d7a8d631a7472d85bbc7cd334c69
Emiliano Ortiz <emiortiz1992@gmail.com>
2020-10-27 10:46:42 -0300

    se inserto imagenes al carrusel

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
     </ol>
     <div class="carousel-inner">
       <div class="carousel-item active">
-          <img src="recursos/img/camion.png" class="embed-responsive" width="100%" height="">
+          <img src="recursos/img/camion1.jpg" class="embed-responsive" width="100%" height="">
         <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"/></svg>
         <div class="container">
           <div class="carousel-caption text-left">
@@ -55,24 +55,24 @@
         </div>
       </div>
       <div class="carousel-item">
+          <img src="recursos/img/camiones.jpg" class="embed-responsive" width="100%" height="auto">
         <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"/></svg>
         <div class="container">
-          <div class="carousel-caption">
-            <h1>Another example headline.</h1>
-            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-            <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
-          </div>
+            <div class="carousel-caption text-left">
+                <h1>SEGURIDAD</h1>
+                <p>Entregando Garlopas a domicilio desde tiempos immemorables. No olvides Registrarte y ser parte de Garlopa Transfer</p>
+            </div>
         </div>
       </div>
       <div class="carousel-item">
+          <img src="recursos/img/carga.jpg" class="embed-responsive" width="100%" height="auto">
         <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"/></svg>
-        <div class="container">
-          <div class="carousel-caption text-right">
-            <h1>One more for good measure.</h1>
-            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-            <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+          <div class="container">
+              <div class="carousel-caption text-left">
+                  <h1>CONFIANZA</h1>
+                  <p>Entregando Garlopas a domicilio desde tiempos immemorables. No olvides Registrarte y ser parte de Garlopa Transfer</p>
+              </div>
           </div>
-        </div>
       </div>
     </div>
     <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
